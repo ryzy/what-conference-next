4d68b56be6f37387d5414a19faec44f3bf3d1382
Marcin Ryzycki <marcin@ryzycki.com>
2018-01-02 17:07:10 +0000

    task: setup @ngrx

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,10 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -14,6 +18,11 @@ import { throwIfAlreadyLoaded } from './core-utils';
     BrowserModule,
     BrowserAnimationsModule,
 
+    StoreModule.forRoot({}),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument({ name: 'what-conference-next.com' }) : [],
+    EffectsModule.forRoot([]),
+
     // Firebase setup
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
@@ -23,7 +32,7 @@ import { throwIfAlreadyLoaded } from './core-utils';
 
     // SW
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-  ]
+  ],
 })
 export class CoreModule {
   public constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
