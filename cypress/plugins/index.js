09a8f704e3629c2c2963ba1e56cb62e42d0adce8
Marcin Ryzycki <marcin@ryzycki.com>
2018-04-22 16:51:18 +0100

    feat(build): add Cypress testing (with cucumber/BDD)

---
+++ cypress/plugins/index.js
@@ -0,0 +1,47 @@
+// const webpackPreProcessor = require('@cypress/webpack-preprocessor');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+
+  // const webpackOptions = {
+  //   resolve: {
+  //     extensions: ['.ts', '.js'],
+  //   },
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.ts$/,
+  //         exclude: [/node_modules/],
+  //         use: [
+  //           {
+  //             loader: 'ts-loader',
+  //           }
+  //         ],
+  //       },
+  //     ],
+  //   },
+  // };
+  //
+
+  // TODO: Cucumber doesn't support Typescript, so... no-go for now ;/
+  // const options = { webpackOptions };
+  // on('file:preprocessor', webpackPreProcessor(options));
+
+  // Must be after webpack, otherwise cucumber files are
+  on('file:preprocessor', cucumber());
+};
