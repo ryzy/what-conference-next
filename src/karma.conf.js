bdf5cb6113a198b30da9e21ad8a852c059d2d770
Marcin Ryzycki <marcin@ryzycki.com>
2018-04-22 19:28:54 +0100

    feat: upgrade to Angular 6

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,27 +1,25 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-mocha-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly', 'text-summary', 'json' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
+      dir: require('path').join(__dirname, '../dist/coverage'),
+      reports: ['html', 'lcovonly', 'text-summary', 'json'],
+      fixWebpackSourcePaths: true,
     },
     reporters: ['mocha', 'kjhtml'],
     port: 9876,
@@ -30,6 +28,6 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     browserNoActivityTimeout: 30000, // increase from default 10s due to sporadic timeouts on CI
-    singleRun: false
+    singleRun: false,
   });
 };
